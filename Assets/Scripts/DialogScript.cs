ff9d8a883ce1ec249bff39c3a28cab6b55b0d631
BilliBoi122 <efuchs2006@gmail.com>
2023-08-15 15:26:37 +0200

    trigger stuff working

---
+++ Assets/Scripts/DialogScript.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    //Panel must be created on the canvas and then dragged into this section in the inspector.
+    public GameObject panel;
+
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            panel.SetActive(true);
+            Debug.Log("trigger was triggered");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //if (collision.gameObject.tag != "Player")
+        //{
+            panel.SetActive(false);
+            Debug.Log("trigger was trigger'nted");
+        //}
+    }
+
+    /*Alternative
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            panel.SetActive(true);
+            Debug.Log("trigger was triggered");
+        }
+    }
+    */
+}
+
