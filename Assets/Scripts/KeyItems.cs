cd49680e24f7db9bea5a9b6dfc8c6ccb448fd209
BilliBoi122 <efuchs2006@gmail.com>
2023-09-18 16:50:11 +0200

    keyItems WIP

--- Assets/Scripts/KeyItems.cs
+++ Assets/Scripts/KeyItems.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class SlotManager
+{
+    public Button slot;
+    public string item;
+}
 public class KeyItems : MonoBehaviour
     {
-        public Button Slots;
-        public string item;
-    public List<KeyItems> slotsList = new List<KeyItems>();
-        void Start()
-        {
-            //Slot1.Select();
-           // Slot1.onClick.AddListener(PickItem);
-        }
-        void Update()
-        {
-            
-        }
-
-        void PickItem()
-        {
-            ChangeSelected(1);
-        }
-
-        void ChangeSelected(int slotNum)
-        {
-        //selected = itemTest;
-        }
+    public SlotManager slotManager;
+    public List<SlotManager> slotsList = new List<SlotManager>();
+    void Start()
+    {
+    slotsList[0].slot.Select();
+    for (int i = 1; i<=7;i++) slotsList[i].slot.onClick.AddListener(() => SelectItem(i));
     }
+    void Update()
+    {
+        
+    }
+
+    void SelectItem(int slotNum)
+    {
+        Debug.Log(slotNum);
+    }
+}
