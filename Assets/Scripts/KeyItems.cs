e3fabe1938bc4a5056b33c552493b6d946754029
BilliBoi122 <efuchs2006@gmail.com>
2023-09-18 12:35:44 +0200

    yes vrey sneaky indeed

---
+++ Assets/Scripts/KeyItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyItems : MonoBehaviour
+    {
+        public Button Slots;
+        public string item;
+    public List<KeyItems> slotsList = new List<KeyItems>();
+        void Start()
+        {
+            //Slot1.Select();
+           // Slot1.onClick.AddListener(PickItem);
+        }
+        void Update()
+        {
+            
+        }
+
+        void PickItem()
+        {
+            ChangeSelected(1);
+        }
+
+        void ChangeSelected(int slotNum)
+        {
+        //selected = itemTest;
+        }
+    }
