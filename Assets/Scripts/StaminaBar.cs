dee727775d52fdd17cade4d93007c44f37d0377a
BilliBoi122 <efuchs2006@gmail.com>
2023-10-18 10:15:42 +0200

    bug fix and tilemap setup

--- Assets/Scripts/StaminaBar.cs
+++ Assets/Scripts/StaminaBar.cs
@@ -3,11 +3,10 @@ using UnityEngine.UI;
 public class StaminaBar : MonoBehaviour
 {
     public Image StaminaBarLeft, StaminaBarRight;
-    private float currentBarOpacity;
-    private float targetOpacity = 0f;
+    public float currentBarOpacity;
+    public float targetOpacity = 0f;
     public PlayerController playerController;
     public float stamina;
-    //public RendererListDesc objectRenderer;
     private float currentValue = 0;
     private bool canSprint;
 
@@ -16,7 +15,9 @@ public class StaminaBar : MonoBehaviour
         StaminaBarLeft = GameObject.Find("StaminaBarLeft").GetComponent<Image>();
         StaminaBarRight = GameObject.Find("StaminaBarRight").GetComponent<Image>();
         playerController = GameObject.Find("Lilia").GetComponent<PlayerController>();
-        targetOpacity = 0;
+        StaminaBarLeft.color = new Color(1, 1, 1, 0);
+        StaminaBarRight.color = new Color(1, 1, 1, 0);
+
     }
     // Update is called once per frame
     void Update()
@@ -30,17 +31,17 @@ public class StaminaBar : MonoBehaviour
         if (stamina >= 50) canSprint = true;
         if (stamina <= 1) canSprint = false;
 
-        if (stamina <= 100)
+        if (stamina < 99)
         {
             if (canSprint) currentValue = 1 - stamina / 50;
-            else  if (canSprint) currentValue = 1;
+            else if (canSprint) currentValue = 1;
             currentValue = Mathf.Clamp01(currentValue);
             Color lerpedColour = Color.Lerp(Color.white, Color.red, currentValue);
             StaminaBarLeft.color = lerpedColour;
             StaminaBarRight.color = lerpedColour;
         }
         // turning transparent
-        
+
         currentBarOpacity = StaminaBarLeft.color.a;
         if (stamina >= 100f) targetOpacity = 0;
         else targetOpacity = 1;
