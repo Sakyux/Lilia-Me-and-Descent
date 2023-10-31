cd2490ae6e446e64d3f421396ef3c77c6c341507
BilliBoi122 <efuchs2006@gmail.com>
2023-10-31 11:08:07 +0200

    added a remove timer for log entries

--- Assets/Scripts/GameLog.cs
+++ Assets/Scripts/GameLog.cs
@@ -8,10 +8,23 @@ public class GameLog : MonoBehaviour
     private List<string> Eventlog = new List<string>();
     private string guiText = "";
     private Rect LogBox = new Rect(0, 0, Screen.width/2, Screen.height);
+    private float time = 0;
 
     // Public VARS
     public int maxLines = 5;
-
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 3)
+        {
+            time = 0;
+            if (Eventlog.Count != 0)
+            {
+                Eventlog.RemoveAt(0);
+                AddEvent(null);
+            }
+        }
+    }
     void OnGUI()
     {
         GUI.Label(LogBox, guiText, GUI.skin.label);
@@ -19,7 +32,7 @@ public class GameLog : MonoBehaviour
 
     public void AddEvent(string eventString)
     {
-        Eventlog.Add(eventString + "\n");
+        if (eventString != null) Eventlog.Add(eventString + "\n");
 
         if (Eventlog.Count >= maxLines)
             Eventlog.RemoveAt(0);
@@ -31,5 +44,6 @@ public class GameLog : MonoBehaviour
             guiText += logEvent;
             guiText += "";
         }
+        time = 0;
     }
 }
